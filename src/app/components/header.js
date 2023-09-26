79330183bc3a267b908fc49489e21d478200790c
Sam Ullman <samullman@gmail.com>
2023-09-26 06:46:49 -0500

    spacing

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -4,26 +4,40 @@ import MySidebar from "./sidebar";
 
 function Header() {
   return (
-    <Box position="fixed" zIndex="1000" width="100vw" bg="white" py={2} px={5}>
+    <div>
       <Box
-        maxWidth="1000px"
-        margin="0 auto"
-        display="flex"
-        justifyContent={"space-between"}
-        alignItems="baseline"
+        position="fixed"
+        zIndex="1000"
+        width="100vw"
+        bg="white"
+        py={2}
+        px={5}
       >
-        <Box title="title" pt="2px">
-          <Link href="/" passHref>
-            {" "}
-            <ChakraLink>Huck Finn Ventures</ChakraLink>
-          </Link>
-        </Box>
+        <Box
+          maxWidth="1000px"
+          margin="0 auto"
+          display="flex"
+          justifyContent={"space-between"}
+          alignItems="baseline"
+        >
+          <Box title="title" pt="2px">
+            <Link href="/" passHref>
+              {" "}
+              <ChakraLink>Huck Finn Ventures</ChakraLink>
+            </Link>
+          </Box>
 
-        <Box title="hamburger">
-          <MySidebar />
+          <Box title="hamburger">
+            <MySidebar />
+          </Box>
         </Box>
       </Box>
-    </Box>
+
+      <Box>
+        py={2}
+        px={5}
+      </Box>
+    </div>
   );
 }
 
